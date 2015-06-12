d1ac9f4ae66e53877024bfc98c5843f6258c3792
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2015-06-12 12:34:52 +0300

    PlainView output parameter

--- src/main/java/com/egnyte/utils/auditreporter/Runner.java
+++ src/main/java/com/egnyte/utils/auditreporter/Runner.java
@@ -5,16 +5,21 @@ import com.egnyte.utils.auditreporter.domain.UserTupleToPojo;
 import com.egnyte.utils.auditreporter.view.PlainView;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
 
 public class Runner {
+    private static final Charset CS = Charset.forName("UTF-8");
+
     public static void main(String[] args) throws IOException {
         final String usersFile = args[0];
         final String filesFile = args[0];
         final CsvLoader loader = new CsvLoader();
-        new PlainView().render(new GroupFilesByUser().group(
-                new ObjectsFromCsv<>(loader, new UserTupleToPojo())
-                        .apply(usersFile),
-                new ObjectsFromCsv<>(loader, new FileInfoTupleToPojo())
-                        .apply(filesFile)));
+        new PlainView(new OutputStreamWriter(System.out, CS))
+                .render(new GroupFilesByUser().group(
+                        new ObjectsFromCsv<>(loader, new UserTupleToPojo())
+                                .apply(usersFile),
+                        new ObjectsFromCsv<>(loader, new FileInfoTupleToPojo())
+                                .apply(filesFile)));
     }
 }
--- src/main/java/com/egnyte/utils/auditreporter/view/PlainView.java
+++ src/main/java/com/egnyte/utils/auditreporter/view/PlainView.java
@@ -4,11 +4,20 @@ import com.egnyte.utils.auditreporter.domain.FileInfo;
 import com.egnyte.utils.auditreporter.domain.User;
 import com.google.common.collect.Multimap;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 /**
  * Daneel Yaitskov
  */
 public class PlainView {
-    public void render(Multimap<User, FileInfo> data) {
+    private final OutputStreamWriter out;
+
+    public PlainView(OutputStreamWriter out) {
+        this.out = out;
+    }
+
+    public void render(Multimap<User, FileInfo> data) throws IOException {
         printHeader();
         for (User user : data.keySet()) {
             printUserHeader(user.login);
@@ -18,16 +27,16 @@ public class PlainView {
         }
     }
 
-    private void printHeader() {
-        System.out.println("Audit Report");
-        System.out.println("============");
+    private void printHeader() throws IOException {
+        out.write("Audit Report\n");
+        out.write("============\n");
     }
 
-    private void printUserHeader(String userName) {
-        System.out.println("## User: " + userName);
+    private void printUserHeader(String userName) throws IOException {
+        out.write("## User: " + userName + "\n");
     }
 
-    private void printFile(String fileName, long fileSize) {
-        System.out.println("* " + fileName + " ==> " + fileSize + " bytes");
+    private void printFile(String fileName, long fileSize) throws IOException {
+        out.write("* " + fileName + " ==> " + fileSize + " bytes\n");
     }
 }
